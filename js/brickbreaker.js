dfe8a722681303b0790077db46346fd8647d8c51
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2020-03-01 17:36:52 -0500

    51

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 let counter = document.getElementById("wins")
@@ -41,6 +40,7 @@ function init() {
     createblocks();
     start = true;
 }
+
 function game() {
     if (start) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -109,59 +109,91 @@ function checkCollision() {
             break;
         }
     }
-    if (boinker.y + boinker.radius == user.y) {
-           let a = 3;
-           const speed_change_change = a / 25;
-           for (let i = 2; i <= 100; i += 2) {
-               if (boinker.x >= user.x - boinker.radius + i - 2 && boinker.x < user.x - boinker.radius + i) {
-                   if (i < 50) {
-                       boinker.up = true;
-                       boinker.right = false;
-                       speed = (speed >= 11) ? speed = 11 : speed + 0.5;
-                       speed_change = Math.abs(a);
-                   }
-                   else if (i >= 50) {
-                       boinker.up = true;
-                       boinker.right = true;
-                       speed = (speed >= 11) ? speed = 11 : speed + 0.5;
-                       speed_change = Math.abs(a);
-                   }
-                   break;
-               }
-               else {
-                   a -= speed_change_change;
-               }
-           }
-       }
 
-       if (user.x + user.width > canvas.width) {
-           user.movement = 0;
-           user.x = canvas.width - user.width;
-       }
-       else if (user.x < 0) {
-           user.movement = 0;
-           user.x = 0;
-       }
-       else {
-           user.movement = 20;
-       }
-   }
-   function draw() {
-       ctx.strokeRect(user.x, user.y, user.width, user.height);
-       ctx.beginPath();
-       ctx.arc(boinker.x, boinker.y, boinker.radius, 0, Math.PI * 2);
-       ctx.stroke();
-       for (let i = 0; i < blocks.length; i++) {
-           ctx.strokeRect(blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height);
-       }
-   }
-   function getArrowKeys(event) {
-       if (start) {
-           if (event.keyCode == 37) {
-               moveuser(-1 * user.movement);
-           }
-           else if (event.keyCode == 39) {
-               moveuser(user.movement);
-           }
-       }
-   }
+    if (boinker.y + boinker.radius == user.y) {
+        let a = 3;
+        const speed_change_change = a / 25;
+        for (let i = 2; i <= 100; i += 2) {
+            if (boinker.x >= user.x - boinker.radius + i - 2 && boinker.x < user.x - boinker.radius + i) {
+                if (i < 50) {
+                    boinker.up = true;
+                    boinker.right = false;
+                    speed = (speed >= 11) ? speed = 11 : speed + 0.5;
+                    speed_change = Math.abs(a);
+                }
+                else if (i >= 50) {
+                    boinker.up = true;
+                    boinker.right = true;
+                    speed = (speed >= 11) ? speed = 11 : speed + 0.5;
+                    speed_change = Math.abs(a);
+                }
+                break;
+            }
+            else {
+                a -= speed_change_change;
+            }
+        }
+    }
+
+    if (user.x + user.width > canvas.width) {
+        user.movement = 0;
+        user.x = canvas.width - user.width;
+    }
+    else if (user.x < 0) {
+        user.movement = 0;
+        user.x = 0;
+    }
+    else {
+        user.movement = 20;
+    }
+}
+function draw() {
+    ctx.strokeRect(user.x, user.y, user.width, user.height);
+    ctx.beginPath();
+    ctx.arc(boinker.x, boinker.y, boinker.radius, 0, Math.PI * 2);
+    ctx.stroke();
+    for (let i = 0; i < blocks.length; i++) {
+        ctx.strokeRect(blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height);
+    }
+}
+function getArrowKeys(event) {
+    if (start) {
+        if (event.keyCode == 37) {
+            moveuser(-1 * user.movement);
+        }
+        else if (event.keyCode == 39) {
+            moveuser(user.movement);
+        }
+    }
+}
+function moveuser(pixels) {
+    user.x += pixels;
+}
+function createblocks() {
+    for (let y = 0; y <= 80; y += 40) {
+        for (let x = 0; x < canvas.width; x += canvas.width / 10) {
+            let bottleTemplate = {
+                x: x,
+                y: y,
+                width: canvas.width / 10,
+                height: 40
+            };
+            blocks.push(bottleTemplate);
+        }
+    }
+}
+function lose() {
+    init();
+}
+function win() {
+  start = false;
+  wins += 1
+  counter.innerHTML = wins
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "blue";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2.5;
+  ctx.textAlign = "center";
+  ctx.font = "60px Arial";
+  ctx.fillText("You Win!", canvas.width/2, (canvas.height/2) - 40);
+}
