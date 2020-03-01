dd2b70062eea610d5e2f5147c543502da4f4802d
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2020-03-01 17:35:14 -0500

    it dont make sense

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,9 +1,43 @@
-///////////////////// CONSTANTS /////////////////////////////////////
 
-///////////////////// APP STATE (VARIABLES) /////////////////////////
-
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
-///////////////////// EVENT LISTENERS ///////////////////////////////
-
-///////////////////// FUNCTIONS /////////////////////////////////////
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+let counter = document.getElementById("wins")
+let speed;
+ctx.strokeStyle = "blue";
+let speed_change;
+let start = false;
+let blocks = [];
+let wins = 0;
+let user = {
+    x: (canvas.width / 2) - 40,
+    y: canvas.height - 10,
+    width: 80,
+    height: 5,
+    movement: 20
+};
+let boinker = {
+    x: undefined,
+    y: undefined,
+    radius: 10,
+    right: true,
+    up: true
+};
+window.onload = function() {
+    document.getElementById("reset-button").onclick = init;
+    document.getElementById("game").onclick = init;
+    game();
+}
+document.addEventListener("keydown", getArrowKeys);
+function init() {
+    boinker.x = canvas.width / 2;
+    boinker.y = canvas.height - 20;
+    boinker.right = true;
+    boinker.up = true;
+    user.x = (canvas.width / 2) - 40;
+    user.y = canvas.height - 10;
+    speed_change = 1;
+    speed = 0;
+    blocks = [];
+    createblocks();
+    start = true;
+}
